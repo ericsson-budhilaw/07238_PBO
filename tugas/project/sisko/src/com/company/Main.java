ac623cee6a173784e59e15186e827367da7a37bf
Dimas Tri Martono <72393510+dimastrim45@users.noreply.github.com>
2020-11-14 10:57:06 +0700

    Update Main.java
    Update Main.java
    + Menambhkan MainMenu pilihan Guru dan Siswa
    untuk pilihan siswa akan panggil siswa();
    untuk pilihan Guru akan panggil guru();
    
    + Menambahkan guru():
          +[1] Lihat Guru
          +[2] Edit Guru
          +[3] Tambah Guru
          +[4] Hapus Guru
          +[0] Kembali
    + Menambahkan readGuru()
    + Menambahkan updateGuru()
    + Menambahkan addGuru()
    + Menambahkan deleteGuru(nip)

--- tugas/project/sisko/src/com/company/Main.java
+++ tugas/project/sisko/src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     // Update git
     public ArrayList<Siswa> dataSiswa   = new ArrayList();
+    public ArrayList<Guru> dataGuru   = new ArrayList();
     public Scanner input;
 
     public static void main(String[] args) {
@@ -16,6 +17,33 @@ public class Main {
 
     public void menu() {
         input = new Scanner(System.in);
+        int pilMain;
+
+        do {
+            System.out.println("Selamat Datang Di Sistem Informasi Sekolah");
+            System.out.println("Pilihan: ");
+            System.out.println("1. Siswa");
+            System.out.println("2. Guru");
+            System.out.print("Masukkan Pilihan : ");
+            pilMain = input.nextInt();
+            switch(pilMain){
+                case 1:
+                    siswa();
+                    break;
+                case 2:
+                    guru();
+                    break;
+                default:
+                    System.out.println("Pilihan Anda Salah!!!");
+                    break;
+            }
+            
+            
+        } while(pilMain != 3);
+    }
+    
+    //Section Siswa
+    public void siswa(){
         int selectedMenu, nis;
 
         do {
@@ -114,6 +142,124 @@ public class Main {
                 if(dataSiswa.get(i).getNIS() == nis) {
                     System.out.println("Data siswa milik: " + dataSiswa.get(i).getNama() + " berhasil terhapus");
                     dataSiswa.remove(i);
+                }
+            }
+        }
+    }
+    
+    //Section Guru
+    public void guru(){
+        int selectMenu, nip;
+        do{
+            System.out.println("===Manajemen Guru===");
+            System.out.println("[1] Lihat Guru");
+            System.out.println("[2] Edit Guru");
+            System.out.println("[3] Tambah Guru");
+            System.out.println("[4] Hapus Guru");
+            System.out.println("[0] Kembali");
+            System.out.println("======================");
+            System.out.print("Pilihan: ");
+
+            selectMenu = input.nextInt();
+            input.nextLine();
+
+            switch(selectMenu){
+                case 1:
+                    readGuru();
+                    break;
+
+                case 2:
+                    System.out.println("Masukan NIP: ");
+                    nip = input.nextInt();
+                    updateGuru(nip);
+                    break;
+
+                case 3:
+                    addGuru();
+                    break;
+
+                case 4:
+                    System.out.println("Masukkan NIP: (Jika hapus semua data masukkan 0) ");
+                    nip = input.nextInt();
+                    deleteGuru(nip);
+                    break;
+
+                case 0:
+                    break;
+
+                default:
+                    System.out.println("Salah Menu!!!");
+                   // backToMenu();
+                    break;
+            }
+        }while(selectMenu!=5);
+    }
+    public void addGuru() {
+        System.out.print("Masukkan Nama: ");
+        String nama = input.next();
+        System.out.print("Masukkan NIK: ");
+        String nik = input.next();
+        System.out.print("Masukkan NIP: ");
+        int nip = input.nextInt();
+        System.out.print("Masukkan NUPTK: ");
+        int  nuptk = input.nextInt();
+        System.out.print("Jenis Kelamin: ");
+        String JK = input.next();
+        System.out.print("Masukkan Kelas: ");
+        String kelas = input.next();
+        
+        dataGuru.add(new Guru(nama, nik, nip, nuptk, kelas));
+    }
+
+    public void updateGuru(int nip) {
+        for(int i = 0; i < dataGuru.size(); i++) {
+            if(dataGuru.get(i).getNIP() == nip) {
+                System.out.println("============");
+                System.out.println("Nama: " + dataGuru.get(i).getNama());
+                System.out.println("NIK: "+ dataGuru.get(i).getNIK());
+                System.out.println("NIP: " + dataGuru.get(i).getNIP());
+                System.out.println("NUPTK: " + dataGuru.get(i).getNUPTK());
+                System.out.println("============");
+                
+                System.out.print("Masukkan Nama: ");
+                String nama = input.next();
+                System.out.print("Masukkan NIK: ");
+                String nik = input.next();
+                System.out.print("Masukkan NIP: ");
+                int nipNEW = input.nextInt();
+                System.out.print("Masukkan NUPTK: ");
+                int  nuptk = input.nextInt();
+                System.out.print("Masukkan kelas: ");
+                String kelas = input.next();
+
+                // Menggunakan add karena user harus menginputkan kembali semua data
+                dataGuru.set(i, new Guru(nama, nik, nipNEW, nuptk, kelas));
+            }
+        }
+        
+    }
+
+    public void readGuru() {
+        for(int i = 0; i < dataGuru.size(); i++) {
+            System.out.println("============");
+            System.out.println("Nama: " + dataGuru.get(i).getNama());
+            System.out.println("NIK: "+ dataGuru.get(i).getNIK());
+            System.out.println("NIP: " + dataGuru.get(i).getNIP());
+            System.out.println("NUPTK: " + dataGuru.get(i).getNUPTK());
+            System.out.println("Kelas: "+ dataGuru.get(i).getKelas()+ " [ " + dataGuru.get(i).getJadwalKelas() + " ]");
+            System.out.println("============");
+        }
+    }
+
+    public void deleteGuru(int nip) {
+        if(nip == 0) {
+            dataGuru.clear();
+            System.out.println("Data berhasil dihapus");
+        } else {
+            for(int i = 0; i < dataGuru.size(); i++) {
+                if(dataGuru.get(i).getNIP() == nip) {
+                    System.out.println("Data Guru milik: " + dataGuru.get(i).getNama() + " berhasil terhapus");
+                    dataGuru.remove(i);
                 }
             }
         }
